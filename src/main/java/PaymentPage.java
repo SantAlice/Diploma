8107810cc0b8f57614ba5e04a8efe09daa35e410
Anastasia <90128050+SantAlice@users.noreply.github.com>
2023-11-11 22:28:08 +0300

    created methods

--- src/main/java/PaymentPage.java
+++ src/main/java/PaymentPage.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.SelenideElement;
+
+import java.time.Duration;
 import java.util.List;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -33,5 +35,71 @@ public class PaymentPage {
         $$(".button__content").find(exactText("Купить в кредит")).click();
         $$(".heading_theme_alfa-on-white").find(exactText("Кредит по данным карты")).shouldBe(visible);
     }
+
+    public void checkSuccessMessage() {
+        $$(".notification__title")
+                .find(exactText("Успешно"))
+                .shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    public void checkErrorMessage() {
+        $$(".notification__title")
+                .find(exactText("Ошибка"))
+                .shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    public void checkWrongFormatMessage() {
+        $$(".input__sub")
+                .find(exactText("Неверный формат"))
+                .shouldBe(visible);
+    }
+
+    public void checkExpiredMessage() {
+        $$(".input__sub")
+                .find(exactText("Истёк срок действия карты"))
+                .shouldBe(visible);
+    }
+
+    public void checkEmptyMessage() {
+        $$(".input__sub")
+                .find(exactText("Поле обязательно для заполнения"))
+                .shouldBe(visible);
+    }
+
+    public void setCardNumber(String cNumber) {
+        cardNumber.setValue(cNumber);
+    }
+
+    public void setCardMonth(String cMonth) {
+        month.setValue(cMonth);
+    }
+
+    public void setCardYear(String cYear) {
+        year.setValue(cYear);
+    }
+
+    public void setCardOwner(String cOwner) {
+        cardOwner.setValue(cOwner);
+    }
+
+    public void setCardCVV(String cCvv) {
+        cvcOrCvvNumber.setValue(cCvv);
+    }
+
+    public void pushContinueButton() {
+        findButtonByText("Продолжить").click();
+    }
+
+    private SelenideElement findButtonByText(String buttonText) {
+        return $$(".button__content")
+                .find(exactText(buttonText))
+                .shouldBe(visible);
+    }
+
+
+
+
+
+
 }
 
